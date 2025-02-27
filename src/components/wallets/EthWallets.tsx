9622c1fa1d71f3cef83d1e2d09f5c75601978d05
AslamThachapalli <aslam.develop912@gmail.com>
2025-02-27 17:07:42 +0530

    balance fetching

--- src/components/wallets/EthWallets.tsx
+++ src/components/wallets/EthWallets.tsx
@@ -1,8 +1,29 @@
-import { Wallet } from 'ethers';
+import { Wallet } from '@/lib/wallet';
+import { Wallet as EthWallet } from 'ethers';
+import { useEffect, useState } from 'react';
 
-export default function EthWallets({ wallet }: { wallet: Wallet | null }) {
+export default function EthWallets({ wallet }: { wallet: EthWallet | null }) {
+    const [balance, setBalance] = useState('');
+
+    useEffect(() => {
+        if (wallet) {
+            Wallet.getBalanceEth(wallet.address).then((balance) => {
+                setBalance(String(balance));
+            });
+        }
+    }, []);
+
     if (!wallet) {
         return <>No Wallet</>;
     }
-    return <>{wallet.address}</>;
+
+    return (
+        <div className="flex flex-col gap-4 justify-center items-center h-[50vh]">
+            <div className='flex flex-col justify-center items-center'>
+                <p className='font-semibolc text-4xl'>{balance}</p>
+                <p>Coins Available</p>
+            </div>
+            <div>{wallet.address}</div>
+        </div>
+    );
 }
--- src/lib/wallet.ts
+++ src/lib/wallet.ts
@@ -1,34 +1,74 @@
-import { mnemonicToSeedSync } from "bip39";
-import slip10 from "micro-key-producer/slip10.js";
-import { Keypair } from "@solana/web3.js";
-import { DerivationPath } from "./utils";
-import { Wallet as EthWallet, HDNodeWallet } from "ethers";
+import { mnemonicToSeedSync } from 'bip39';
+import slip10 from 'micro-key-producer/slip10.js';
+import { Keypair } from '@solana/web3.js';
+import { DerivationPath } from './utils';
+import { Wallet as EthWallet, HDNodeWallet } from 'ethers';
 
 export class Wallet {
-  static createForSolana(account: number): Keypair {
-    const mnemonics = localStorage.getItem("mnemonic");
-    if (!mnemonics) {
-      throw Error("No mnemonic found!");
-    }
-    const seed = mnemonicToSeedSync(mnemonics);
-    const hdkey = slip10.fromMasterSeed(seed.toString("hex"));
+    static createForSolana(account: number): Keypair {
+        const mnemonics = localStorage.getItem('mnemonic');
+        if (!mnemonics) {
+            throw Error('No mnemonic found!');
+        }
+        const seed = mnemonicToSeedSync(mnemonics);
+        const hdkey = slip10.fromMasterSeed(seed.toString('hex'));
 
-    const keyPair = Keypair.fromSeed(
-      hdkey.derive(DerivationPath.solana(account)).privateKey
-    );
-    return keyPair;
-  }
-
-  static createForEth(account: number): EthWallet {
-    const mnemonics = localStorage.getItem("mnemonic");
-    if (!mnemonics) {
-      throw Error("No mnemonic found!");
+        const keyPair = Keypair.fromSeed(
+            hdkey.derive(DerivationPath.solana(account)).privateKey,
+        );
+        return keyPair;
     }
-    const seed = mnemonicToSeedSync(mnemonics);
-    const hdNode = HDNodeWallet.fromSeed(seed);
-    const child = hdNode.derivePath(DerivationPath.etherium(account));
-    const privateKey = child.privateKey;
-    const wallet = new EthWallet(privateKey);
-    return wallet;
-  }
+
+    static createForEth(account: number): EthWallet {
+        const mnemonics = localStorage.getItem('mnemonic');
+        if (!mnemonics) {
+            throw Error('No mnemonic found!');
+        }
+        const seed = mnemonicToSeedSync(mnemonics);
+        const hdNode = HDNodeWallet.fromSeed(seed);
+        const child = hdNode.derivePath(DerivationPath.etherium(account));
+        const privateKey = child.privateKey;
+        const wallet = new EthWallet(privateKey);
+        return wallet;
+    }
+
+    static async getBalanceEth(publicKey: string): Promise<number> {
+        const res = await fetch(
+            'https://eth-mainnet.g.alchemy.com/v2/P4G854hHX1l69UZRRfDQeOq9dTjqrnsZ',
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    jsonrpc: '2.0',
+                    id: 1,
+                    method: 'eth_getBalance',
+                    params: [publicKey, 'latest'],
+                }),
+            },
+        );
+
+        const resJson = await res.json();
+
+        const balance = resJson.result;
+        return Number(balance) / Math.pow(10, 18);
+    }
+
+    static async getBalanceSol(publicKey: string): Promise<number> {
+        const res = await fetch(
+            'https://solana-mainnet.g.alchemy.com/v2/P4G854hHX1l69UZRRfDQeOq9dTjqrnsZ',
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    jsonrpc: '2.0',
+                    id: 1,
+                    method: 'getBalance',
+                    params: [publicKey],
+                }),
+            },
+        );
+
+        const resJson = await res.json();
+
+        const balance = resJson.result.value;
+        return balance / Math.pow(10, 9);
+    }
 }
